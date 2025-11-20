7c780b7e8b8232999a28c1a6ec9fd8dc486eb51a
Shnkr <shankarnavgire702@gmail.com>
2025-11-20 22:50:40 +0530

    fix: add type annotation in VisualBuilder

--- components/VisualBuilder.tsx
+++ components/VisualBuilder.tsx
@@ -1,39 +1,35 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-export default function VisualBuilder(){
-  const [nodes,setNodes]=useState([]);
+type NodeType = {
+  id: number;
+  type: string;
+};
 
-  function add(type){
-    setNodes([...nodes, {
-      id: Date.now(),
-      type,
-      x: 100,
-      y: 100,
-      text: type==="text" ? "Your Text" : ""
-    }]);
-  }
+export default function VisualBuilder() {
+  const [nodes, setNodes] = useState<NodeType[]>([]);
 
-  function move(id,e){
-    const updated = nodes.map(n => n.id===id ? {...n, x:e.clientX/2, y:e.clientY/2} : n);
-    setNodes(updated);
+  function add(type: string) {
+    setNodes([
+      ...nodes,
+      {
+        id: Date.now(),
+        type,
+      },
+    ]);
   }
 
   return (
-    <div className="relative bg-white w-full h-[600px] rounded-xl shadow overflow-hidden">
-      {nodes.map(n=>(
-        <div key={n.id}
-          draggable
-          onDrag={e=>move(n.id,e)}
-          className="absolute p-3 bg-black text-white rounded"
-          style={{top:n.y,left:n.x}}>
-          {n.text || n.type}
-        </div>
-      ))}
-      <div className="absolute bottom-5 left-5 flex gap-4">
-        <button onClick={()=>add('text')} className="p-2 bg-black text-white rounded">Add Text</button>
-        <button onClick={()=>add('icon')} className="p-2 bg-black text-white rounded">Add Icon</button>
-        <button onClick={()=>add('shape')} className="p-2 bg-black text-white rounded">Add Shape</button>
+    <div>
+      <button onClick={() => add("text")}>Add Text</button>
+      <button onClick={() => add("image")}>Add Image</button>
+
+      <div style={{ marginTop: 20 }}>
+        {nodes.map((n) => (
+          <div key={n.id}>
+            Node: {n.type} (ID: {n.id})
+          </div>
+        ))}
       </div>
     </div>
   );
